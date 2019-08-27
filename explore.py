ac21a6f01b6057e859cc187527b7af9caf7dd95f
Ricardo Arango Giraldo <ricardo.arango@riarangogi.com>
2019-08-27 10:15:40 -0500

    I created the explore.py file

---
+++ explore.py
@@ -0,0 +1,8 @@
+"""
+Author: Ricardo Arango Giraldo
+Date: 27/Aug/2019
+Description: In  this script we understand the data and discovery relationship
+between the variables
+OS: elementary OS 5.0 Juno (64-bit), Basado en Ubuntu 18.04.2 LTS
+Python Version: Anaconda 3.7.3
+"""
