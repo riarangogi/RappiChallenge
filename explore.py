202b39952358456a7805ad861baa1f6a9a758230
Ricardo Arango Giraldo <ricardo.arango@riarangogi.com>
2019-08-28 00:14:37 -0500

    I added the hour plot

--- explore.py
+++ explore.py
@@ -10,40 +10,53 @@ import pandas as pd
 import datetime as dt
 import matplotlib.pyplot as plt
 
+#cargar el dataset
 data = pd.read_csv("./Data/Raw/orders.csv")
+#leer la columna created_at como una fecha
 data["created_at"] = pd.to_datetime(data["created_at"])
 print(data.head())
 print(data.info())
 
+#subconjunto con las variables numericas
 numericData = data[["to_user_distance", "to_user_elevation", "total_earning"]]
 print(numericData.describe())
 
+#funcion para graficar las variables numericas
 def plotNumericData(variable, title):
-
+#graficar el histografica
 	fig = plt.figure()
 	ax1 = plt.subplot(1, 2, 1)
 	ax1.set_title("Histograma {}".format(title))
 	numericData[variable].plot(kind="hist")
-
+#graficar el box plot
 	ax2 = plt.subplot(1, 2, 2)
 	ax2.set_title("Box plot {}".format(title))
 	numericData[variable].plot(kind="box")
-
+#mostrar grafica
 	plt.show()
 
 
+#graficar la variable to_user_distance
 plotNumericData("to_user_distance", "de la distancia al usuario")
-
+#graficar la variable to_user_elevation
 plotNumericData("to_user_elevation", "de la diferencia en altura del usuario")
-
+#graficar la  variable total_earning
 plotNumericData("total_earning", "del valor por el servicio de mensajeria")
 
+#grafica de barras de la variable taken
 plt.title("Cantidad de ordenes aceptadas y ordenes no tomadas")
 takenTable = data["taken"].value_counts()
 takenTable.plot(kind="bar")
 plt.show()
 
+#grafica de barras de la cantidad de servicios por dia
 plt.title("Cantidad de solicitudes por día")
 dateTable = data["created_at"].dt.weekday.value_counts()
+dateTable.plot(kind="bar")
+plt.show()
+
+#grafica de barras de la cantidad de servicios por hora
+plt.title("Cantidad de solicitudes por hora")
+dateTable = data["created_at"].dt.hour.value_counts()
 dateTable.plot(kind="bar")
 plt.show()
