e430b1801504cf2a9f98fc2024260914343211c1
Ricardo Arango Giraldo <ricardo.arango@riarangogi.com>
2019-08-28 17:26:27 -0500

    ok

--- modify.py
+++ modify.py
@@ -11,9 +11,41 @@ import numpy as np
 
 data = pd.read_csv("./Data/Raw/orders.csv")
 data["created_at"] = pd.to_datetime(data["created_at"])
-
 data["hour"] = data["created_at"].dt.hour
+tmp = []
 
-data["primeTime"] = np.where((data["hour"]<=19) & (data["hour"]>=11), 1, 0)
+print(data.info())
 
-print(data)
+toUserDistanceMedian = data["to_user_distance"].median()
+IQR = data["to_user_distance"].quantile(0.75)-data["to_user_distance"].quantile(0.25)
+LS = data["to_user_distance"].quantile(0.75)+1.5*IQR
+LI = data["to_user_distance"].quantile(0.25)-1.5*IQR
+
+print(LS, LI, toUserDistanceMedian)
+"""
+df = pd.DataFrame({"a": [1,2,3], "b": [5,5,7], "c": ["q", "a", "p"]})
+col = []
+col.reshape(3, 1)
+df["d"]=col
+print(df)
+print(len(df))
+for i in range(len(df)):
+	if (df.loc[i, "a"]>=2):
+		col.append(df.loc[i, "c"])
+	else:
+		col.append(0)
+
+print(col)
+
+"""
+
+for i in range(len(data)):
+	if (data.loc[i, "to_user_distance"]>=LS):
+		tmp.append(toUserDistanceMedian)
+	if (data.loc[i, "to_user_distance"]<=LI):
+		tmp.append(toUserDistanceMedian)
+	else:
+		tmp.append(data.loc[i,"to_user_distance"])
+
+print(len(tmp))
+print(data.head())
