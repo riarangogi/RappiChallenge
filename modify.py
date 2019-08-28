0a566da4f0404fb8b0df223e5e38ed4f7f1fde79
Ricardo Arango Giraldo <ricardo.arango@riarangogi.com>
2019-08-28 09:13:23 -0500

    I created the primeTime column with hours between 11 and 19

--- modify.py
+++ modify.py
@@ -7,5 +7,13 @@ OS: elementary OS 5.0 Juno (64-bit), Basado en Ubuntu 18.04.2 LTS
 Python Version: Anaconda 3.7.3
 """
 import pandas as pd
+import numpy as np
 
-data = 
+data = pd.read_csv("./Data/Raw/orders.csv")
+data["created_at"] = pd.to_datetime(data["created_at"])
+
+data["hour"] = data["created_at"].dt.hour
+
+data["primeTime"] = np.where((data["hour"]<=19) & (data["hour"]>=11), 1, 0)
+
+print(data)
