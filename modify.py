afda2e4bc911c63277a3255e8a138d24ebbf9acd
Ricardo Arango Giraldo <ricardo.arango@riarangogi.com>
2019-08-28 01:53:44 -0500

    I created the modify.py file

---
+++ modify.py
@@ -0,0 +1,11 @@
+"""
+Author: Ricardo Arango Giraldo
+Date: 27/Aug/2019
+Description: In  this script we create and transform variables in preparation
+for data modeling.
+OS: elementary OS 5.0 Juno (64-bit), Basado en Ubuntu 18.04.2 LTS
+Python Version: Anaconda 3.7.3
+"""
+import pandas as pd
+
+data = 
