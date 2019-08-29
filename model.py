8fda5d6438cab900f4704d30c661a98df7cb6658
Ricardo Arango Giraldo <ricardo.arango@riarangogi.com>
2019-08-29 01:26:44 -0500

    I fit the first model

--- model.py
+++ model.py
@@ -10,6 +10,7 @@ Python Version: Anaconda 3.7.3
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
 
 np.random.seed(47)
 df =  pd.read_csv("./Data/Tidy/dfFile.csv")
@@ -18,4 +19,6 @@ y = df["taken"]
 X = df.drop("taken", axis=1)
 X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.3, random_state=47)
 
-
+logreg = LogisticRegression()
+logreg.fit(X_train,y_train)
+y_pred=logreg.predict(X_test)
