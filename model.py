eb951d264bcd62a7b34c3a02618520090843611f
Ricardo Arango Giraldo <ricardo.arango@riarangogi.com>
2019-08-29 02:07:19 -0500

    I evaluated the first logistic model

--- model.py
+++ model.py
@@ -9,8 +9,11 @@ Python Version: Anaconda 3.7.3
 """
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn import metrics
 
 np.random.seed(47)
 df =  pd.read_csv("./Data/Tidy/dfFile.csv")
@@ -22,3 +25,28 @@ X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.3, random_st
 logreg = LogisticRegression()
 logreg.fit(X_train,y_train)
 y_pred=logreg.predict(X_test)
+
+y_pred_proba = logreg.predict_proba(X_test)[::,1]
+fpr, tpr, _ = metrics.roc_curve(y_test,  y_pred_proba)
+auc = metrics.roc_auc_score(y_test, y_pred_proba)
+plt.plot(fpr,tpr,label="modelo 1, auc="+str(auc))
+plt.legend(loc=4)
+plt.show()
+
+cnf_matrix = metrics.confusion_matrix(y_test, y_pred)
+class_names=[0,1]
+fig, ax = plt.subplots()
+tick_marks = np.arange(len(class_names))
+plt.xticks(tick_marks, class_names)
+plt.yticks(tick_marks, class_names)
+sns.heatmap(pd.DataFrame(cnf_matrix), annot=True, cmap="YlGnBu" ,fmt='g')
+ax.xaxis.set_label_position("top")
+plt.tight_layout()
+plt.title('Confusion matrix', y=1.1)
+plt.ylabel('Actual label')
+plt.xlabel('Predicted label')
+plt.show()
+
+print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+print("Precision:",metrics.precision_score(y_test, y_pred))
+print("Recall:",metrics.recall_score(y_test, y_pred))
