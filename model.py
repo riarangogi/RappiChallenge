9d0a3964bfcfd0e45264f8cacd92bc6fe74b4ed3
Ricardo Arango Giraldo <ricardo.arango@riarangogi.com>
2019-08-29 00:25:19 -0500

    I created model.py file

---
+++ model.py
@@ -0,0 +1,9 @@
+"""
+Author: Ricardo Arango Giraldo
+Date: 28/Aug/2019
+Description: In  this script we create  various models (data mining)
+techniques on the prepared variables in order to create models that possibly
+provide the desired outcome.
+OS: elementary OS 5.0 Juno (64-bit), Basado en Ubuntu 18.04.2 LTS
+Python Version: Anaconda 3.7.3
+"""
