82ce7d82cfb5fa61f6295ba0a2a378478916786c
Ricardo Arango Giraldo <ricardo.arango@riarangogi.com>
2019-08-28 22:05:30 -0500

    I created toUserElevation column

--- modify.py
+++ modify.py
@@ -13,32 +13,11 @@ data = pd.read_csv("./Data/Raw/orders.csv")
 data["created_at"] = pd.to_datetime(data["created_at"])
 data["hour"] = data["created_at"].dt.hour
 data["primeTime"] = np.where((data["hour"]<=19) & (data["hour"]>=11), 1, 0)
-tmp = []
-
-print(data.info())
 
 toUserDistanceMedian = data["to_user_distance"].median()
 IQR = data["to_user_distance"].quantile(0.75)-data["to_user_distance"].quantile(0.25)
 LS = data["to_user_distance"].quantile(0.75)+1.5*IQR
 LI = data["to_user_distance"].quantile(0.25)-1.5*IQR
-
-print(LS, LI, toUserDistanceMedian)
-"""
-df = pd.DataFrame({"a": [1,2,3], "b": [5,5,7], "c": ["q", "a", "p"]})
-col = []
-col.reshape(3, 1)
-df["d"]=col
-print(df)
-print(len(df))
-for i in range(len(df)):
-	if (df.loc[i, "a"]>=2):
-		col.append(df.loc[i, "c"])
-	else:
-		col.append(0)
-
-print(col)
-
-print(len(tmp))"""
 
 for i in range(len(data)):
 	if (data.loc[i, "to_user_distance"]>=LS):
@@ -46,7 +25,34 @@ for i in range(len(data)):
 	if (data.loc[i, "to_user_distance"]<=LI):
 		data.loc[i, "toUserDistance"] = toUserDistanceMedian
 	else:
-
 		data.loc[i, "toUserDistance"] = data.loc[i,"to_user_distance"]
 
-print(data.head())
+
+toUserElevationMedian = data["to_user_elevation"].median()
+IQR = data["to_user_elevation"].quantile(0.75)-data["to_user_elevation"].quantile(0.25)
+LS = data["to_user_elevation"].quantile(0.75)+1.5*IQR
+LI = data["to_user_elevation"].quantile(0.25)-1.5*IQR
+
+for i in range(len(data)):
+	if (data.loc[i, "to_user_elevation"]>=LS):
+		data.loc[i, "toUserElevation"] = toUserElevationMedian
+	if (data.loc[i, "to_user_elevation"]<=LI):
+		data.loc[i, "toUserElevation"] = toUserElevationMedian
+	else:
+		data.loc[i, "toUserElevation"] = data.loc[i,"to_user_elevation"]
+
+
+toUserElevationMedian = data["to_user_elevation"].median()
+IQR = data["to_user_elevation"].quantile(0.75)-data["to_user_elevation"].quantile(0.25)
+LS = data["to_user_elevation"].quantile(0.75)+1.5*IQR
+LI = data["to_user_elevation"].quantile(0.25)-1.5*IQR
+
+for i in range(len(data)):
+	if (data.loc[i, "to_user_elevation"]>=LS):
+		data.loc[i, "toUserElevation"] = toUserElevationMedian
+	if (data.loc[i, "to_user_elevation"]<=LI):
+		data.loc[i, "toUserElevation"] = toUserElevationMedian
+	else:
+		data.loc[i, "toUserElevation"] = data.loc[i,"to_user_elevation"]
+
+
